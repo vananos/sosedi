be66d1ac6b0a1fce1e9675af47f0b0c86767d489
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-23 14:19:48 +0000

    Deploy vananos/sosedi to github.com/vananos/sosedi.git:gh-pages

---
+++ precache-manifest.7c74a5ade7169e3f204fe5798eae8517.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf32bf7d3f344bcd5261",
+    "url": "/static/css/main.9d3e43d9.chunk.css"
+  },
+  {
+    "revision": "cf32bf7d3f344bcd5261",
+    "url": "/static/js/main.f340b446.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "24a638611829f7bd2597",
+    "url": "/static/js/2.08747fd9.chunk.js"
+  },
+  {
+    "revision": "d9a24d05eed3db4d122adcaf0ac3d956",
+    "url": "/static/media/info-circle-solid.d9a24d05.svg"
+  },
+  {
+    "revision": "c297db2bd84eca98a27b48bb17adbdb3",
+    "url": "/static/media/user-regular.c297db2b.svg"
+  },
+  {
+    "revision": "ae1651a4d6861b71cb8e80156e8e1c03",
+    "url": "/static/media/plus-solid.ae1651a4.svg"
+  },
+  {
+    "revision": "c8813cf40f12659b37410cd71f3292c7",
+    "url": "/static/media/cogs-solid.c8813cf4.svg"
+  },
+  {
+    "revision": "8d759d57df90f5924dced096910b9593",
+    "url": "/static/media/comment-solid.8d759d57.svg"
+  },
+  {
+    "revision": "758d25b7b05ef39dad1db659b95d0455",
+    "url": "/static/media/user-solid.758d25b7.svg"
+  },
+  {
+    "revision": "72dfa22613d4c455d516e113a638c88b",
+    "url": "/static/media/scroll-solid.72dfa226.svg"
+  },
+  {
+    "revision": "05cbc704dcf6bd77e036480cb7294ee9",
+    "url": "/static/media/headset-solid.05cbc704.svg"
+  },
+  {
+    "revision": "4485f8865ab57c59cc2d7555eb8254a2",
+    "url": "/static/media/question-circle-solid.4485f886.svg"
+  },
+  {
+    "revision": "fcfb77cf352d7e7552e375084b8cb3bd",
+    "url": "/static/media/check-solid.fcfb77cf.svg"
+  },
+  {
+    "revision": "a5513b28396cc2794cce1cd587f6a9fb",
+    "url": "/index.html"
+  }
+];
