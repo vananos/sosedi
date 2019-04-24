a6945f2839f89ddb85ad5897b56c76e72fe5ed87
vananos <zaiatcivan@gmail.com>
2019-04-24 15:14:10 +0300

    fix slider

--- src/components/common/Slider.js
+++ src/components/common/Slider.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./Slider.css";
+import "./Slider.scss";
 
 export default class Slider extends Component {
   constructor(props) {
@@ -13,40 +13,62 @@ export default class Slider extends Component {
   render() {
     const { min, max } = this.props;
     const { currentMin, currentMax } = this.state;
+    const rangeSize = max - min;
+
     return (
       <div className="multi-range">
-        <input
-          type="range"
-          min={min}
-          max={max}
-          name="min-value"
-          value={currentMin}
-          onChange={e => {
-            const newValue = +e.target.value;
-            console.log(newValue);
-            if (newValue < this.state.currentMax) {
-              this.setState({
-                currentMin: newValue
-              });
-            }
-          }}
-        />
-        <output className="slider-hint" style={{left: ((currentMin - min )/ max) * 100 + '%'}}>{currentMin}</output>
-        <input
-          type="range"
-          min={min}
-          max={max}
-          value={currentMax}
-          onChange={e => {
-            const newValue = +e.target.value;
-            if (newValue > this.state.currentMin) {
-              this.setState({
-                currentMax: newValue
-              });
-            }
-          }}
-        />
-        <output className="slider-hint" style={{left: ((currentMax - min )/ max) * 100 - 2 + '%', top: "-15px"}}>{currentMax}</output>
+        <div>
+          <input
+            type="range"
+            min={min}
+            max={max}
+            name="min-value"
+            step="1"
+            value={currentMin}
+            onChange={e => {
+              const newValue = +e.target.value;
+              const newState = {
+                currentMin: newValue >= max ? max - 1 : newValue
+              };
+              if (newValue >= this.state.currentMax) {
+                newState.currentMax = newState.currentMin + 1;
+              }
+              this.setState(newState);
+            }}
+          />
+          <output
+            className="slider-hint"
+            style={{
+              left: `calc(${((currentMin - min) / rangeSize) * 100}% - 0.8em)`
+            }}
+          >
+            {currentMin}
+          </output>
+          <input
+            type="range"
+            min={min}
+            max={max}
+            value={currentMax}
+            onChange={e => {
+              const newValue = +e.target.value;
+              const newState = {
+                currentMax: newValue <= min ? +min + 1 : newValue
+              };
+              if (newValue <= this.state.currentMin) {
+                newState.currentMin = newState.currentMax - 1;
+              }
+              this.setState(newState);
+            }}
+          />
+          <output
+            className="slider-hint"
+            style={{
+              left: `calc(${((currentMax - min) / rangeSize) * 100}% + 4px)`
+            }}
+          >
+            {currentMax}
+          </output>
+        </div>
       </div>
     );
   }
