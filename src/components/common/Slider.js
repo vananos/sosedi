2a97ffdb94c526c67f70cc37af14569d6a7aea69
vananos <zaiatcivan@gmail.com>
2019-04-27 13:24:56 +0300

    slider behavior improvement

--- src/components/common/Slider.js
+++ src/components/common/Slider.js
@@ -10,6 +10,42 @@ export default class Slider extends Component {
     };
   }
 
+  minValueChange = newValue => {
+    const { max, double } = this.props;
+    const newState = {
+      currentMin: double && newValue >= max ? max - 1 : newValue
+    };
+    if (double && newValue >= this.state.currentMax) {
+      newState.currentMax = newState.currentMin + 1;
+    }
+    this.setState(newState);
+  };
+
+  maxValueChange = newValue => {
+    const { min } = this.props;
+    const newState = {
+      currentMax: newValue <= min ? +min + 1 : newValue
+    };
+    if (newValue <= this.state.currentMin) {
+      newState.currentMin = newState.currentMax - 1;
+    }
+    this.setState(newState);
+  };
+
+  handleMinUnselectedAreaClick = e => {
+    const elementWidth = e.nativeEvent.target.offsetWidth;
+    const scale = elementWidth / (this.state.currentMin - this.props.min);
+    const newCurrentMin = e.nativeEvent.offsetX / scale;
+    this.minValueChange(Math.round(newCurrentMin));
+  };
+
+  handleMaxUnselectedAreaClick = e => {
+    const elementWidth = e.nativeEvent.target.offsetWidth;
+    const scale = elementWidth / (this.props.max - this.state.currentMax);
+    const newCurrentMin = e.nativeEvent.offsetX / scale + this.state.currentMax;
+    this.maxValueChange(Math.round(newCurrentMin));
+  };
+
   render() {
     const { min, max, double } = this.props;
     const { currentMin, currentMax } = this.state;
@@ -19,7 +55,11 @@ export default class Slider extends Component {
     return (
       <div className="multi-range">
         <div>
-          <span className="unselected" style={{ width: `${minOffset}%` }} />
+          <span
+            className="unselected"
+            style={{ width: `${minOffset}%` }}
+            onClick={this.handleMinUnselectedAreaClick}
+          />
           <input
             type="range"
             min={min}
@@ -27,16 +67,7 @@ export default class Slider extends Component {
             name="min-value"
             step="1"
             value={currentMin}
-            onChange={e => {
-              const newValue = +e.target.value;
-              const newState = {
-                currentMin: double && newValue >= max ? max - 1 : newValue
-              };
-              if (double && newValue >= this.state.currentMax) {
-                newState.currentMax = newState.currentMin + 1;
-              }
-              this.setState(newState);
-            }}
+            onChange={e => this.minValueChange(+e.target.value)}
           />
           <output
             className="slider-hint"
@@ -48,22 +79,17 @@ export default class Slider extends Component {
           </output>
           {double ? (
             <React.Fragment>
-              <span className="unselected" style={{ width: `calc(100% - ${maxOffset}%)`, right: '0' }} />
+              <span
+                className="unselected"
+                style={{ width: `calc(100% - ${maxOffset}%)`, right: "0" }}
+                onClick={this.handleMaxUnselectedAreaClick}
+              />
               <input
                 type="range"
                 min={min}
                 max={max}
                 value={currentMax}
-                onChange={e => {
-                  const newValue = +e.target.value;
-                  const newState = {
-                    currentMax: newValue <= min ? +min + 1 : newValue
-                  };
-                  if (newValue <= this.state.currentMin) {
-                    newState.currentMin = newState.currentMax - 1;
-                  }
-                  this.setState(newState);
-                }}
+                onChange={e => this.maxValueChange(+e.target.value)}
               />
               <output
                 className="slider-hint"
